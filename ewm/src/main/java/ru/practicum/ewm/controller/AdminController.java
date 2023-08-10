79e5d3759cdf46940a4de12227a4ad14fc1856e0
barabebr <unior1234@yandex.ru>
2023-08-10 08:36:00 +0400

    feat: AdminController user get, delete

--- ewm/src/main/java/ru/practicum/ewm/controller/AdminController.java
+++ ewm/src/main/java/ru/practicum/ewm/controller/AdminController.java
@@ -1,14 +1,12 @@
 package ru.practicum.ewm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.practicum.ewm.service.interfaces.IUserService;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.models.user.UserDto;
+import ru.practicum.ewm.service.interfaces.IUserService;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
@@ -21,10 +19,19 @@ public class AdminController {
         this.userService = userService;
     }
 
-
     @PostMapping("/users")
     public UserDto add(@Valid @RequestBody UserDto user) {
         return userService.add(user);
+    }
+
+    @GetMapping("/users")
+    public List<UserDto> get(List<Long> usersId) {
+        return userService.getUsers(usersId);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public void delete(@PathVariable Long userId) {
+        userService.delete(userId);
     }
 
 }
