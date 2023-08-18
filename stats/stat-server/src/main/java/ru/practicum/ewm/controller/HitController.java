1ea852394494918af1f888bd31a534886e7d52d3
bababebr <unior1234@yandex.ru>
2023-08-18 19:28:28 +0600

    Test

--- stats/stat-server/src/main/java/ru/practicum/ewm/controller/HitController.java
+++ stats/stat-server/src/main/java/ru/practicum/ewm/controller/HitController.java
@@ -2,6 +2,7 @@ package ru.practicum.ewm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.ewm.dto.HitDto;
 import ru.practicum.ewm.dto.HitDtoShort;
@@ -22,13 +23,14 @@ public class HitController {
     }
 
     @PostMapping("/hit")
+    @ResponseStatus(HttpStatus.CREATED)
     public HitDto post(@RequestBody HitDto dto) {
         return service.add(dto);
     }
 
     @GetMapping("/stats")
-    public List<HitDtoShort> get(@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
-                                 @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+    public List<HitDtoShort> get(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                 @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
                                  @RequestParam(required = false) List<String> uris,
                                  @RequestParam(defaultValue = "false") boolean unique) {
         return service.get(start, end, uris, unique);
